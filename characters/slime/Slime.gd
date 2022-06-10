4a2a188e40f5a76edf6cbd00ffba978a5714e99c
Leonardo Cserny <leo.cserny@openbet.com>
2022-06-10 10:36:53 +0300

    adds wander controller

--- characters/slime/Slime.gd
+++ characters/slime/Slime.gd
@@ -18,12 +18,14 @@ export var ACCELERATION = 500
 export var MAX_SPEED = 50
 export var KNOCKBACK = 150
 export var PUSHBACK = 800
+export var MIN_WANDER = 4
 
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var animatedSprite = $AnimatedSprite
 onready var stats = $Stats
 onready var hurtbox = $Hurtbox
 onready var softCollision = $SoftCollision
+onready var wanderController= $WanderController
 
 func _ready():
 	animatedSprite.playing = true
@@ -36,25 +38,46 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 			seek_player()
+			alter_wander_state()
 		WANDER:
-			pass
+			seek_player()
+			alter_wander_state()
+			velocity = get_velocity_towards(delta, wanderController.target_position)
+			if global_position.distance_to(wanderController.target_position) <= MIN_WANDER:
+				update_wander_state()
 		CHASE:
 			var player = playerDetectionZone.player
 			if player != null:
-				var direction = (player.global_position - global_position).normalized()
-				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+				velocity = get_velocity_towards(delta, player.global_position)
 			else:
 				state = IDLE
-			animatedSprite.flip_h = velocity.x < 0
 	
 	var push_vector = softCollision.get_push_vector()
 	if softCollision.is_colliding(push_vector):
 		velocity += push_vector * delta * PUSHBACK
 	velocity = move_and_slide(velocity)
 
+func get_velocity_towards(delta, position):
+	var direction = global_position.direction_to(position)
+	var new_velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+	animatedSprite.flip_h = new_velocity.x < 0
+	return new_velocity
+
 func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE
+
+func alter_wander_state():
+	if wanderController.get_time_left() == 0:
+		update_wander_state()
+
+func update_wander_state():
+	state = choose_random_state([IDLE, WANDER])
+	wanderController.set_wander_time(rand_range(1, 3))
+
+func choose_random_state(states: Array):
+	states.shuffle()
+	return states.pop_front()
 
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
